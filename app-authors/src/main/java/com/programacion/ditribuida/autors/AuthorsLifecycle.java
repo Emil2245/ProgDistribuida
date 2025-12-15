5b44cb0515221b82a187cfb7a75e1607deac20ce
Emil2245 <emilmontiel@hotmail.com>
2025-12-15 08:39:22 -0500

    feat: tags traefik, pending

--- app-authors/src/main/java/com/programacion/ditribuida/autors/AuthorsLifecycle.java
+++ app-authors/src/main/java/com/programacion/ditribuida/autors/AuthorsLifecycle.java
@@ -3,6 +3,7 @@ package com.programacion.ditribuida.autors;
 import io.quarkus.runtime.ShutdownEvent;
 import io.quarkus.runtime.StartupEvent;
 import io.vertx.core.Vertx;
+import io.vertx.ext.consul.CheckOptions;
 import io.vertx.ext.consul.ConsulClient;
 import io.vertx.ext.consul.ConsulClientOptions;
 import io.vertx.ext.consul.ServiceOptions;
@@ -10,6 +11,9 @@ import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.event.Observes;
 import jakarta.inject.Inject;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+import java.net.InetAddress;
+import java.util.List;
 
 @ApplicationScoped
 public class AuthorsLifecycle {
@@ -33,13 +37,25 @@ public class AuthorsLifecycle {
                     .setHost(consulHost)
                     .setPort(consulPort);
 
+            var ipAddress = InetAddress.getLocalHost().getHostAddress();
+            var urlcheck = String.format("http://%s:%d/ping", ipAddress, appPort);
+            var checkOptions = new CheckOptions().setHttp(urlcheck).setInterval("10s").setDeregisterAfter("10s");
+
             ConsulClient client = ConsulClient.create(vertx, options);
+
+            var tags = List.of("traefik.enable= true",
+                    "traefik.http.routers.authors.rule=PathPrefix(`/app-authors`)",
+                    "traefik.http.middlewares.authors-stripprefix.stripPrefix.prefixes=/app-authors",
+                    "traefik.http.routers.authors.middlewares=authors-stripprefix"
+            );
 
             ServiceOptions serviceOptions = new ServiceOptions()
                     .setName("app-authors")
                     .setId("app-authors-" + appPort)
-                    .setAddress("127.0.0.1")
-                    .setPort(appPort);
+                    .setAddress(ipAddress)
+                    .setPort(appPort)
+                    .setCheckOptions(checkOptions)
+                    .setTags(tags);
 
             client.registerService(serviceOptions).onSuccess(it -> {
                 System.out.println("Service registered: " + serviceOptions.getName());
@@ -55,21 +71,17 @@ public class AuthorsLifecycle {
         System.out.println("*******************AuthorsLifecycle stop()");
         try {
 
-            ConsulClientOptions options = new ConsulClientOptions()
-                    .setHost(consulHost)
-                    .setPort(consulPort);
+            ConsulClientOptions options = new ConsulClientOptions().setHost(consulHost).setPort(consulPort);
 
             ConsulClient client = ConsulClient.create(vertx, options);
 
             String serviceId = "app-authors-" + appPort;
 
-            client.deregisterService(serviceId)
-                    .onSuccess(it -> {
-                        System.out.println("Service deregistered: " + serviceId);
-                    })
-                    .onFailure(err -> {
-                        System.out.println("Service deregistration failed: " + err.getMessage());
-                    });
+            client.deregisterService(serviceId).onSuccess(it -> {
+                System.out.println("Service deregistered: " + serviceId);
+            }).onFailure(err -> {
+                System.out.println("Service deregistration failed: " + err.getMessage());
+            });
         } catch (Exception e) {
             e.printStackTrace();
         }
--- app-books/src/main/java/com/programacion/ditribuida/books/BooksLifecycle.java
+++ app-books/src/main/java/com/programacion/ditribuida/books/BooksLifecycle.java
@@ -3,6 +3,7 @@ package com.programacion.ditribuida.books;
 import io.quarkus.runtime.ShutdownEvent;
 import io.quarkus.runtime.StartupEvent;
 import io.vertx.core.Vertx;
+import io.vertx.ext.consul.CheckOptions;
 import io.vertx.ext.consul.ConsulClient;
 import io.vertx.ext.consul.ConsulClientOptions;
 import io.vertx.ext.consul.ServiceOptions;
@@ -10,6 +11,9 @@ import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.event.Observes;
 import jakarta.inject.Inject;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+import java.net.InetAddress;
+import java.util.List;
 
 @ApplicationScoped
 public class BooksLifecycle {
@@ -34,13 +38,25 @@ public class BooksLifecycle {
                     .setHost(consulHost)
                     .setPort(consulPort);
 
+            var ipAddress = InetAddress.getLocalHost().getHostAddress();
+            var urlcheck = String.format("http://%s:%d/ping", ipAddress, appPort);
+            var checkOptions = new CheckOptions().setHttp(urlcheck).setInterval("10s").setDeregisterAfter("10s");
+
+            var tags = List.of("traefik.enable= true",
+                    "traefik.http.routers.books.rule=PathPrefix(`/app-books`)",
+                    "traefik.http.middlewares.books-stripprefix.stripPrefix.prefixes=/app-books",
+                    "traefik.http.routers.books.middlewares=books-stripprefix"
+            );
+
             ConsulClient client = ConsulClient.create(vertx, options);
 
             ServiceOptions serviceOptions = new ServiceOptions()
                     .setName("app-books")
                     .setId("app-books-" + appPort)
-                    .setAddress("127.0.0.1")
-                    .setPort(appPort);
+                    .setAddress(ipAddress)
+                    .setPort(appPort)
+                    .setCheckOptions(checkOptions)
+                    .setTags(tags);
 
             client.registerService(serviceOptions).onSuccess(it -> {
                 System.out.println("Service registered: " + serviceOptions.getName());
